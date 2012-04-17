401f90ee34d7029bbcfe762116fdc538766af2e3
Lee Faus <leefaus@me.com>
2012-04-16 20:39:11 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resurchr::Application.config.secret_token = 'a8eb9c50e4ca5cb89c27d536d3e2faa3ebb2da7af5ffe7d8c176cbd48e0764baade46aa58c54202b070d51c1f3d3dc291cadf62d2a85b6a0ac7174419bd2a45c'
